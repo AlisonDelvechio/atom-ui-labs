3d252cc20d864fca29d701f7ea1e50fb36dd9ff2
Alison Delvechio <alisondelvechio@outlook.com>
2025-06-10 22:33:44 -0300

    Primeiro componente atom-button para entendimento do projeto

--- demo-app-angular/src/app/app.component.ts
+++ demo-app-angular/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppComponent {}
+
+export class AppComponent {
+  handleStencilClick() {
+    console.log('Botão Stencil clicado!');
+    alert('Você clicou no botão Stencil!');
+  }
+}
